bb7feb6f8ddf039ac4100cde164a8417ba6b20a8
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-10-23 21:12:03 +0700

    fast

---
+++ src/main/java/TicketByPriceAscComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class TicketByPriceAscComparator implements Comparator<Ticket> {
+    public int compare(Ticket o1, Ticket o2) {
+        if (o1.getFlightTime() > o2.getFlightTime()) {
+            return 1;
+        } else if (o1.getFlightTime() < o2.getFlightTime()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
